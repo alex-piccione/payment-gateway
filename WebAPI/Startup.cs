a47b0a66ff9b44a6d4787ef1ae03de2df1d92b33
Alessandro <alessandro.piccione.75@gmail.com>
2019-11-17 13:18:16 +0000

    Add NLog to Web API. Correct namespaces. Remove unused files.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -9,9 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace payment_gateway
+namespace PaymentGateway.WebAPI
 {
     public class Startup
     {
